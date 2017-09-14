1eef714d8b1a0a9bdd4bfe33ea02ff594ab757d6
Ryan Hungate <ryan@vextras.com>
2017-09-14 13:27:00 -0700

    add curriculum page
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

---
+++ resources/views/pages/curriculum.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('banner')
+    <section id="banner" class="major">
+        <div class="inner">
+            <header class="major">
+                <h1>Workforce Development Program</h1>
+            </header>
+            <div class="content">
+                <p>SKID ROW DENIM ACADEMY provides the formerly homeless Residents with an unmatched experience that builds a proper foundation for employment opportunities allowing for the formerly homeless to give back and support the communities they were once homeless in.</p>
+                <ul class="actions">
+                    <li><a href="#one" class="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('main_content')
+    <div id="main" class="alt">
+
+        <!-- One -->
+        <section id="one">
+            <div class="inner">
+                <header class="major">
+                    <h1>CURRICULUM & SUPPORT</h1>
+                </header>
+                <span class="image main"><img src="images/pic11.jpg" alt="" /></span>
+                <p>SKID ROW DENIM & ACADEMY launched this program with SKID ROW HOUSING TRUST, collectively they implement a three-stage approach to a Workforce Development Program.  That ensures a comprehensive and successful approach for all involved: </p>
+            </div>
+        </section>
+
+    </div>
+@endsection
